01ad76978b493ee04a20a7de2d22b39242d6803b
Ken69420 <dnielmranazam@gmail.com>
2025-04-18 15:27:28 +0800

    add vercel analytics

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Navbar from "./components/Navbar";
 import Sections from "./components/Sections";
 import ParticleBackground from "./components/ParticleBackround";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
         <title>Daniel Imran</title>
       </Head>
       <div className="scroll-smooth relative">
+        <Analytics />
         <ParticleBackground />
         <Navbar />
         <Sections />
